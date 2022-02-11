fb3b3349bc912e445a7c59105cca7183570ab88b
Mitchell Reiff <me@mreiff.dev>
2022-02-11 13:55:41 -0500

    HW2

---
+++ HW2/HW2.cc
@@ -0,0 +1,29 @@
+// I plege my honor that I have abided by the Stevens Honor System.
+// Based on https://docs.opencv.org/4.x/db/deb/tutorial_display_image.html
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char **argv) {
+
+   Mat image = imread("./assets/vivy10.png");
+
+   if (image.empty()) {
+	  cout << "Could not open or find the image" << endl;
+	  return -1;
+   }
+
+   String windowName = "Image Demo";
+
+   namedWindow(windowName);
+
+   imshow(windowName, image);
+
+   waitKey(0);
+   destroyWindow(windowName);
+
+   return 0;
+}
